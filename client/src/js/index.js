d2e005ea62366432cc1ee98b79a24e609141e19a
dopecello <williamlocke.cello@gmail.com>
2022-10-16 19:13:44 -0400

    import compiled css module

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,9 +1,20 @@
+//Import modules
 import "./form";
 import "./submit";
+
+// Import CSS Files
+import "../css/index.css";
+
+//Import imgs
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
+//Import Bootstrap + Popper
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//Add images onLoad
 window.addEventListener("load", function () {
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
