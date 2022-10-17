d8ae69e7ae61dde19f98350dd683b04fdfd39a48
dopecello <williamlocke.cello@gmail.com>
2022-10-16 21:37:13 -0400

    for bcs

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,12 +10,16 @@ import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
+//Import database initializer
+import { initDb } from "./database";
+
 //Import Bootstrap + Popper
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 //Add images onLoad
 window.addEventListener("load", function () {
+  initDb();  
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
