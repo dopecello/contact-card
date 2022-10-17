609e0a44391393a0d16835a381f0e0d4e1309274
dopecello <williamlocke.cello@gmail.com>
2022-10-16 22:21:54 -0400

    implement read and write functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,3 +13,26 @@ export const initDb = async () => {
     },
   });
 };
+
+export const getDb = async () => {
+  const contactDb = await openDB("contact_db", 1);
+  const tx = contactDb.transaction("contacts", "readonly");
+  const store = tx.objectStore("contacts");
+  const request = store.getAll();
+  const result = await request;
+  return result;
+};
+
+export const postDb = async (name, email, phone, profile) => {
+  const contactDb = await openDB("contact_db", 1);
+  const tx = contactDb.transaction("contacts", "readwrite");
+  const store = tx.objectStore("contacts");
+  const request = store.add({
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
+  const result = await request;
+  console.log("data saved to db!", result);
+};
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -11,7 +11,7 @@ import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
 //Import database initializer
-import { initDb } from "./database";
+import { initDb, getDb, postDb } from "./database";
 
 //Import Bootstrap + Popper
 import { Tooltip, Toast, Popover } from "bootstrap";
@@ -19,7 +19,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 //Add images onLoad
 window.addEventListener("load", function () {
-  initDb();  
+  initDb();
+  getDb();
+  postDb("dopecello", "blahblah@gmail.com", 3019999916, "Bear");
+  getDb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
